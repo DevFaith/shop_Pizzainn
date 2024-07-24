3353ce2e2fe00eb3a97829e40453dcb7d160738f
DevFaith <waitherafaith@outlook.com>
2024-07-24 21:15:07 +0300

    * feat(Home.jsx): add Home component to project

---
+++ projo/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className='headerContainer'>
+            <h1>PizzaHut</h1>
+            <p>Pizza to fit any taste</p>
+            <button>Order Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
