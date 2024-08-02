f7086d8c999fe088da591e7055a9663d02699dc3
DevFaith <waitherafaith@outlook.com>
2024-08-02 23:32:16 +0300

    * feat(Addproducts.jsx): add functionality to add products to the database * * This commit adds a new file `Addproducts.jsx` which contains a React component for adding products to the database. The component uses Firebase for storing and retrieving data. It includes form inputs for entering the product name, price, and image. When the form is submitted, the component uploads the image to Firebase storage, retrieves the download URL, and adds the product details (name, price, and image URL) to the `products` collection in the Firestore database. If there is an error during the process, it is displayed on the page. After successfully adding the product, the user is navigated to the `/menu` page.

---
+++ projo/src/Admindashboard/Addproducts.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { db, storage } from '../Firebase/Config'
+import { useNavigate } from 'react-router-dom'
+import {collection, addDoc} from 'firebase/firestore'
+import { ref,uploadBytes, getDownloadURL } from 'firebase/storage'
+function Addproducts() {
+    const [Name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [image, setImage] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+    const handleproduct = async (e)=> {
+        e.preventDefault()
+        try {
+        // imagestorage 
+         const imageRef = ref(storage, 'productsimage'+ image.name);
+          await uploadBytes(imageRef, image)
+        
+          const imageurl = await getDownloadURL(imageRef);
+
+          await addDoc(collection(db, 'products'), {
+               name: Name,
+               price: price,
+               image: imageurl
+           })
+           navigate('/menu')
+        } catch (error) {
+          setError(error)
+        }
+    }
+
+  return (
+    <div>
+      <h1>{error}</h1>
+      <form onSubmit={handleproduct}>
+        <input type="text"  placeholder='Enter product name' onChange={(e)=> setName (e.target.value)}/>
+        <input type="number" placeholder='Enter product price' onChange={(e)=> setPrice (e.target.value)} />
+        <input type="file"  placeholder='Enter product image' onChange={(e)=> setImage (e.target.files[0])}/>
+        <button>Add Product</button>
+      </form>
+    </div>
+  )
+}
+
+export default Addproducts
