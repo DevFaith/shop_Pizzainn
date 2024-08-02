c1012af65174466e67a65e3dfdaa2504e8cf5c41
DevFaith <waitherafaith@outlook.com>
2024-08-02 23:33:04 +0300

    * refactor(Menu.jsx): refactor Menu component to fetch products from Firebase and render them dynamically * feat(Menu.jsx): add useEffect hook to fetch products from Firebase on component mount

--- projo/src/Pages/Menu.jsx
+++ projo/src/Pages/Menu.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import Menulist from '../helper/menulist'
+import React, {useEffect, useState} from 'react'
 import Menucard from '../Components/Menucard'
 import '../Styles/Menu.css'
+import { db} from '../Firebase/Config'
+import { getDocs, collection } from 'firebase/firestore'
 
 const Menu = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect (() => {
+    const fetchProducts = async () => {
+      try {
+       const products = await getDocs(collection(db, 'products'))
+       const productsData = products.docs.map(doc =>(doc.data ()))
+      setProducts(productsData)
+      
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProducts()
+  },[])
   return (
+
     <div className="menu">
     <h1 className="menuTitle">Our Menu</h1>
     <div className="menuList">
-      {Menulist.map((menuItem, key) => (
-        <Menucard
-          key={key}
-          name={menuItem.name}
-          image={menuItem.image}
-          price={menuItem.price}
-        />
-      ))}
+      {products.map ((product, index) => 
+      <Menucard name={product.name} price={product.price} image={product.image} key={index}/>
+      )}
     </div>
   </div>
   )
