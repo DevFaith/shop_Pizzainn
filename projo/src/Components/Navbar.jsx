c8b545d039f13f67dd02807955f6a8024a04b8ad
DevFaith <waitherafaith@outlook.com>
2024-08-02 23:32:41 +0300

    * feat(Navbar.jsx): add support for displaying user's name and logout button when user is authenticated * feat(Navbar.jsx): add support for displaying shopping cart and toggle functionality

--- projo/src/Components/Navbar.jsx
+++ projo/src/Components/Navbar.jsx
@@ -4,19 +4,34 @@ import { signOut } from 'firebase/auth'
 import { auth } from '../Firebase/Config'
 import '../Styles/Navbar.css'
 import { Link } from 'react-router-dom'
+import { TiShoppingCart } from "react-icons/ti";
 import { FaUser } from 'react-icons/fa'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../Firebase/Config'
 
 const Navbar = () => {
   const [user, setUser] = useState(null)
+  const [cart, setCart] = useState(false)
 
   useEffect(() => {
-    auth.onAuthStateChanged ((cred) => {
-      console.log(cred);
+    auth.onAuthStateChanged (async(cred) => {
+
+     if (cred) {
+      const user = await getDoc(doc (db, 'users', cred.uid))
+      setUser({name : user.data().name})
+     }
+
+     else {
+      setUser(null)
+     }
     })
-  })
+  }, [])
   const logOut = async () => {
    await signOut(auth) 
   }
+const togglecart = () => {
+  setCart(!cart)
+}
   return (
     <div className='navbar'>
         <div className='left'>
@@ -27,8 +42,15 @@ const Navbar = () => {
             <Link to='/menu'>Menu</Link>
             <Link to='/about'>About</Link>
             <Link to='/contacts'>Contacts</Link>
-            <Link to='/login'><FaUser /></Link>
-            <button onClick={logOut}>Logout</button>
+            {user ? (<><span className='user'>{user.name.charAt(0)}</span> <button onClick={logOut}>Logout</button></>)  : (<Link to='/login'><FaUser /></Link>) }
+            <TiShoppingCart onClick={togglecart}/>
+           
+            
+        </div>
+        {/* cart container */}
+        <div className={cart? 'cart-container active' : 'cart-container'}>
+          <h2>Your Cart</h2>
+          <button onClick={togglecart}>close</button>
         </div>
     </div>
   )
