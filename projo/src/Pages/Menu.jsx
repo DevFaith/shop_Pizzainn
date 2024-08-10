b5d955ef34c0b37472732dcc7580c508e3befc09
DevFaith <waitherafaith@outlook.com>
2024-08-10 12:58:04 +0300

    * refactor(Menu.jsx): clean up code formatting and add missing semicolons * feat(Menu.jsx): add handleCart prop to Menu component to handle adding products to cart

--- projo/src/Pages/Menu.jsx
+++ projo/src/Pages/Menu.jsx
@@ -1,36 +1,42 @@
-import React, {useEffect, useState} from 'react'
-import Menucard from '../Components/Menucard'
-import '../Styles/Menu.css'
-import { db} from '../Firebase/Config'
-import { getDocs, collection } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react';
+import Menucard from '../Components/Menucard';
+import '../Styles/Menu.css';
+import { db } from '../Firebase/Config';
+import { getDocs, collection } from 'firebase/firestore';
 
-const Menu = () => {
-  const [products, setProducts] = useState([])
+const Menu = ({ handleCart }) => {
+  const [products, setProducts] = useState([]);
 
-  useEffect (() => {
+  useEffect(() => {
     const fetchProducts = async () => {
       try {
-       const products = await getDocs(collection(db, 'products'))
-       const productsData = products.docs.map(doc =>(doc.data ()))
-      setProducts(productsData)
-      
+        const productsSnapshot = await getDocs(collection(db, 'products'));
+        const productsData = productsSnapshot.docs.map(doc => doc.data());
+        setProducts(productsData);
       } catch (error) {
-        console.log(error);
+        console.error('Error fetching products:', error);
       }
-    }
-    fetchProducts()
-  },[])
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-
     <div className="menu">
-    <h1 className="menuTitle">Our Menu</h1>
-    <div className="menuList">
-      {products.map ((product, index) => 
-      <Menucard name={product.name} price={product.price} image={product.image} key={index}/>
-      )}
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        {products.map((product, index) => (
+          <Menucard
+            key={index}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            handleCart={() => handleCart(product)} // Pass product to handleCart
+          />
+        ))}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
